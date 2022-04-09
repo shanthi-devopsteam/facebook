14ce38201da297d0d0b2706040500ffb89d69bbe
prashanthi-Devops <prashanthi.msr@gmail.com>
2022-04-09 11:51:34 +0530

    conflict fixed

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 updating in the master branch
 this line updating in dev brancn 
 development updated file
+usage of cherrypick
+this line updating in dev branch
+updated file
 this file contains deploy related information
 using git stash
